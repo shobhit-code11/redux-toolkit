d92f51e48487da2b99ae49f33047ec1bc7795d7d
shobhit-code11 <shobhit2231@gmail.com>
2023-08-29 20:38:56 +0530

    setting up cart where all the added items will come

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.css";
+import {remove} from '../store/CartSlice';
 const Cart = () => {
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const removeToCart = (id) => {
+    // dispatch a remove action
+     dispatch(remove(id));
+  }
+
+  const cards = products.map((product) => (
+    <div className="col-md-12" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "130px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>Rs {product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: "white" }}>
+          <Button variant="danger" onClick={() => removeToCart(product.id)}>
+            Remove Item
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
   return (
     <>
-      <h2>Cart</h2>
+      <div className="row">
+        {cards}
+      </div>
     </>
   );
 };
